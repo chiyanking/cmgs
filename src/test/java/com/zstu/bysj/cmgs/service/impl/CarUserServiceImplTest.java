741d36446c949f8c42bddee3b3777519ff6f3ce6
wangtk <wangtk@yangche51.com>
2017-08-31 16:48:33 +0800

    修改  项目启动和项目测试分块

--- src/test/java/com/zstu/bysj/cmgs/service/impl/CarUserServiceImplTest.java
+++ src/test/java/com/zstu/bysj/cmgs/service/impl/CarUserServiceImplTest.java
@@ -1,10 +1,20 @@
 package com.zstu.bysj.cmgs.service.impl;
 
+import com.zstu.bysj.cmgs.model.entity.AutoBrand;
+import com.zstu.bysj.cmgs.service.AutoBrandService;
 import org.junit.Test;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 public class CarUserServiceImplTest extends BaseTest {
+
+    @Resource
+    AutoBrandService autoBrandService;
+
     @Test
     public void save() throws Exception {
-
+        List<AutoBrand> list = autoBrandService.findList();
+        System.out.println(list);
     }
 }
