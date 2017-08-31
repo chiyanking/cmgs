7ddfaab31777656462eb3f7bfe7866b50faf8673
wangtk <wangtk@yangche51.com>
2017-08-31 14:01:09 +0800

    服务类目更改

--- src/test/java/com/zstu/bysj/cmgs/service/impl/BaseTest.java
+++ src/test/java/com/zstu/bysj/cmgs/service/impl/BaseTest.java
@@ -1,16 +1,11 @@
+package com.zstu.bysj.cmgs.service.impl;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:/spring/spring-*.xml" })
-public class BrandTest {
-
-	@Test
-	public void crawlBrand() {
-
-	}
+public class BaseTest {
 
 }
---
+++ src/test/java/com/zstu/bysj/cmgs/service/impl/CarUserServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.zstu.bysj.cmgs.service.impl;
+
+import org.junit.Test;
+
+public class CarUserServiceImplTest extends BaseTest {
+    @Test
+    public void save() throws Exception {
+
+    }
+}
